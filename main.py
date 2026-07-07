062d84ecff981010f617d1efd905a057f56f0cbf
Ahil634 <2620765G@student.gla.ac.uk>
2026-07-07 23:15:30 +0100

    data_entry get functions added

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import pandas as pd
 import csv
 from datetime import datetime
+from data_entry import get_amount, get_category, get_date, get_description
 
 class CSV:
     CSV_FILE = 'finance_data.csv'
+    COLUMNS = ["date", "amount", "category", "description"]
 
     @classmethod
     def initalise_csv(cls):
         try:
             pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
-            df = pd.DataFrame(columns=["date", "amount", "category", "description"])
+            df = pd.DataFrame(columns=cls.COLUMNS)
             df.to_csv(cls.CSV_FILE, index=False)
 
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description
+        }
+        with open(cls.CSV_FILE, "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+        print("Entry added successfully")
+
 CSV.initalise_csv()
+CSV.add_entry("20-07-2024", 125.65, "Income", "Salary")
